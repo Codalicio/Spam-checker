a5ec42d737aca28afa3555ebc876a63bbd9720e4
Amit Raj <rajamitnine@gmail.com>
2026-01-09 18:20:09 +0530

    Register contact routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes.js");
 const testRoutes = require("./routes/testRoutes.js");
+const contactRoutes = require("./routes/contactRoutes.js");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/test", testRoutes);
+app.use("/contacts", contactRoutes);
 
 module.exports = app;
