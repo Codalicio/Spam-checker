95a95b2867b443a828d04848ed875959e3c10d00
Amit Raj <rajamitnine@gmail.com>
2026-01-09 17:16:41 +0530

    Import and use testRoutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes.js");
+const testRoutes = require("./routes/testRoutes.js");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/test", testRoutes);
 
 module.exports = app;
