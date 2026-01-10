edbe98da87050f1197584775be21a6d6962b5904
Amit Raj <rajamitnine@gmail.com>
2026-01-10 19:51:30 +0530

    Check if the contact already exist

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -5,19 +5,20 @@ const { authMiddleware } = require("../middlewares/authMiddleware.js");
 const router = express.Router();
 
 router.post("/", authMiddleware, async (req, res) => {
-  const { name, phone } = req.body;
-
-  if (!name || !phone) {
-    return res.status(400).json({ message: "name and phone are required!" });
-  }
-
   try {
+    const { name, phone } = req.body;
+    const userId = req.user.userId;
+
+    if (!name || !phone) {
+      return res.status(400).json({ message: "name and phone are required!" });
+    }
+
     const contact = await prisma.contact.create({
       data: { name, phone, ownerId: req.user.userId },
     });
     return res.status(201).json(contact);
   } catch (err) {
-    return res.json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 });
 
