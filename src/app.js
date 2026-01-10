ed8170718d925bb1a6301782878543e679eb7965
Amit Raj <rajamitnine@gmail.com>
2026-01-10 13:43:40 +0530

    Import and use spamReports Router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes.js");
 const testRoutes = require("./routes/testRoutes.js");
 const contactRoutes = require("./routes/contactRoutes.js");
+const spamRoutes = require("./routes/spamRoutes.js");
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/test", testRoutes);
 app.use("/contacts", contactRoutes);
+app.use("/spam", spamRoutes);
 
 module.exports = app;
