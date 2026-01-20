19539ac6036341c280994d164d74a08b65cce5c8
Amit Raj <rajamitnine@gmail.com>
2026-01-20 20:36:07 +0530

    Import and use removeNumberAsSpam controller

--- src/routes/spamRoutes.js
+++ src/routes/spamRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const prisma = require("../client.js");
 const { authMiddleware } = require("../middlewares/authMiddleware.js");
 const { calculateSpamLikelihood } = require("../utils/utils.js");
-const { markNumberAsSpam } = require("../controllers/spam/spamController.js");
+const {
+  markNumberAsSpam,
+  removeNumberAsSpam,
+} = require("../controllers/spam/spamController.js");
 
 const router = express.Router();
 
@@ -10,42 +13,9 @@ const router = express.Router();
 router.post("/", authMiddleware, markNumberAsSpam);
 
 // Remove Spam Report From a Number :
-router.delete("/", authMiddleware, async (req, res) => {
-  try {
-    const { phone } = req.body;
-    const userId = req.user.userId;
+router.delete("/", authMiddleware, removeNumberAsSpam);
 
-    const spamReport = await prisma.spamReport.findFirst({
-      where: {
-        phone,
-        reportedBy: userId,
-      },
-    });
-
-    if (!spamReport) {
-      return res
-        .status(404)
-        .json({ message: "You haven't marked this number as spam" });
-    }
-
-    await prisma.spamReport.delete({
-      where: {
-        id: spamReport.id,
-      },
-    });
-
-    return res.json({
-      success: true,
-      message: "Spam report removed",
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "An error occurred while removing spam report",
-    });
-  }
-});
-
-// Get Spam Report counts and spam-likelihood for a number :
+// Get Spam Report count and spam-likelihood for a number :
 router.get("/:phone", authMiddleware, async (req, res) => {
   try {
     const phone = req.params.phone;
