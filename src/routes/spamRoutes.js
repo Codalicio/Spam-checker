6d048f6cecaac7ba513c674cf0a6505cb1e26f5d
Amit Raj <rajamitnine@gmail.com>
2026-01-20 20:48:30 +0530

    Remove unwanted imports, import findSpamLikelihood and use

--- src/routes/spamRoutes.js
+++ src/routes/spamRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const prisma = require("../client.js");
 const { authMiddleware } = require("../middlewares/authMiddleware.js");
-const { calculateSpamLikelihood } = require("../utils/utils.js");
 const {
   markNumberAsSpam,
   removeNumberAsSpam,
+  findSpamLikelihood,
 } = require("../controllers/spam/spamController.js");
 
 const router = express.Router();
@@ -16,27 +15,6 @@ router.post("/", authMiddleware, markNumberAsSpam);
 router.delete("/", authMiddleware, removeNumberAsSpam);
 
 // Get Spam Report count and spam-likelihood for a number :
-router.get("/:phone", authMiddleware, async (req, res) => {
-  try {
-    const phone = req.params.phone;
-
-    const reportCount = await prisma.spamReport.count({
-      where: { phone },
-    });
-
-    const spamLikelihood = calculateSpamLikelihood(reportCount);
-
-    return res.json({
-      success: true,
-      phone: phone,
-      reportCount: reportCount,
-      spamLikelihood: spamLikelihood,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: "An error occurred while fetching spam information",
-    });
-  }
-});
+router.get("/:phone", authMiddleware, findSpamLikelihood);
 
 module.exports = router;
