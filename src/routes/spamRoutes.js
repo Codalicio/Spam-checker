4925657d8a8d5f1ed8f651b5fd159832297f98f0
Amit Raj <rajamitnine@gmail.com>
2026-01-20 20:24:38 +0530

    Import and use markNumberAsSpam controller

--- src/routes/spamRoutes.js
+++ src/routes/spamRoutes.js
@@ -2,65 +2,12 @@ const express = require("express");
 const prisma = require("../client.js");
 const { authMiddleware } = require("../middlewares/authMiddleware.js");
 const { calculateSpamLikelihood } = require("../utils/utils.js");
+const { markNumberAsSpam } = require("../controllers/spam/spamController.js");
 
 const router = express.Router();
 
-router.post("/", authMiddleware, async (req, res) => {
-  try {
-    const { phone } = req.body;
-    const userId = req.user.userId;
-
-    if (!phone) {
-      return res.status(400).json({ message: "Phone number is required!" });
-    }
-
-    // Check if the phone belongs to the current user(that is logged in)
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      select: { phone: true },
-    });
-
-    if (user && user.phone === phone) {
-      return res
-        .status(400)
-        .json({ message: "You cannot mark your own number as spam!" });
-    }
-
-    const alreadyReported = await prisma.spamReport.findFirst({
-      where: {
-        phone,
-        reportedBy: userId,
-      },
-    });
-
-    if (alreadyReported) {
-      return res
-        .status(400)
-        .json({ message: "You have already marked this number as spam" });
-    }
-
-    await prisma.spamReport.create({
-      data: {
-        phone,
-        reportedBy: userId,
-      },
-    });
-
-    const totalReports = await prisma.spamReport.count({
-      where: { phone },
-    });
-
-    res.status(201).json({
-      success: true,
-      message: "Number marked as spam",
-      totalReports: totalReports,
-    });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "An error occurred while reporting spam" });
-  }
-});
+// Mark a number as spam :
+router.post("/", authMiddleware, markNumberAsSpam);
 
 // Remove Spam Report From a Number :
 router.delete("/", authMiddleware, async (req, res) => {
