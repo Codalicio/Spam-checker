e2af815cbda9d2b4a95ebeae5144869279ded9e2
Amit Raj <rajamitnine@gmail.com>
2026-01-28 19:21:04 +0530

    Import and use serachByPhone controller

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { authMiddleware } = require("../middlewares/authMiddleware.js");
-const { searchByName } = require("../controllers/search/searchController.js");
+const {
+  searchByName,
+  searchByPhone,
+} = require("../controllers/search/searchController.js");
 
 const router = express.Router();
 
 router.get("/name", authMiddleware, searchByName);
+router.get("/phone", authMiddleware, searchByPhone);
 
 module.exports = router;
